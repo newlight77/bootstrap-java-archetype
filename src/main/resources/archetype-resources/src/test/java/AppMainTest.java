810a7520011d628c12743c468ef19d230c287c81
Kong TO <newlight77@users.noreply.github.com>
2019-06-11 11:26:44 +0200

    Create AppMainTest.java

---
+++ src/main/resources/archetype-resources/src/test/java/AppMainTest.java
@@ -0,0 +1,14 @@
+package ${package};
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    void myTest() {
+        assertEquals(true, true);
+    }
+
+}
